dc54910427764a86e224d08f3355d68c78af71ad
Francois <lalisse.francoisgmail.com>
2019-07-22 18:40:02 +0200

    FistCommit

---
+++ addfullevent.py
@@ -0,0 +1,89 @@
+# Refer to the Python quickstart on how to setup the environment:
+# https://developers.google.com/calendar/quickstart/python
+# Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+# stored credentials.
+
+import re
+
+class Event():
+
+
+  def __init__(self,):
+
+    self.TupleOfTask = ["Rien a ajouter", "Sport", "Informatique", 'Loisirs']
+    self.TaskSelected = ""
+    self.Index = [0,1,2,3]
+    self.DateStart = ""
+    self.Location = "Tbd"
+    self.Description =""
+    self.color =""
+    self.occurencefixed=""
+    self.Repeat=''
+
+
+
+  def SetTask(self,):
+    self.Index = input("choissisez entre les loisirs : 1 : Sport , 2 : Informatique , 3 : Loisirs")
+    self.Index = int(self.Index)
+
+    while self.Index != 0 and self.Index != 1 and self.Index != 2 and self.Index != 3:
+      self.Index = input("choissisez entre les loisirs : 1 : Sport , 2 : Informatique , 3 : Loisirs")
+      self.Index = int(self.Index)
+
+    self.TaskSelected = self.TupleOfTask[self.Index]
+    return self.TaskSelected
+
+
+
+  def SetDate(self,):
+    # format de date : 2019-05-28T09:00:00-07:00
+
+    print("+9h de décallage still working on it")
+
+    starttime_regex = "([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})-([0-9]{2}):([0-9]{2})"
+
+    while re.search(starttime_regex, self.DateStart) is None:
+      self.DateStart = input("Entrez une date pour votre tâche sous ce format (année-mois-jourT-heure:minutes:seconde-)")
+
+
+
+  def SetDescription(self,):
+
+    self.Description = input("Entrez une Description pour votre event {}".format(self.TaskSelected))
+
+  def SetColor(self,):
+
+    if self.Index == 1:
+      #color = green
+      self.color = '#d06b64'
+      self.colorID = '2'
+    elif self.Index == 2 :
+      self.color = '#f83a22'
+      self.colorID = '3'
+
+
+    elif self.Index == 3:
+      self.color = '#fa573c'
+      self.colorID = '4'
+
+    return self.color
+    return self.colorID
+
+  def SetRepeat(self,):
+
+    occurence = 'RRULE:FREQ=DAILY;COUNT='
+    self.Repeat = input("comblien de fois par semaine ? ")
+
+    while self.Repeat != "1" and self.Repeat != "2" and self.Repeat != "3" and self.Repeat != "4" and self.Repeat != "5" and self.Repeat != "6 "and self.Repeat != "7":
+      self.Repeat = input("comblien de fois par semaine ? ")
+
+    self.occurencefixed = occurence + self.Repeat
+    print(self.occurencefixed)
+
+
+
+
+
+
+
+
---
+++ quickstart.py
@@ -0,0 +1,116 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from addfullevent import Event
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+
+
+def setupevent():
+
+    SetupEvent = Event()
+    SetupEvent.SetTask()
+    Task = (SetupEvent.TaskSelected)
+
+
+
+
+
+
+
+
+    return addevent(Task)
+
+
+def addevent(Task):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    event = {
+        'summary': Task,
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'colorId': '2',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2019-07-28T16:08:00-07:00',
+            'timeZone': 'Europe/London',
+        },
+
+        'end': {
+            'dateTime': '2019-07-28T17:09:00-07:00',
+            'timeZone': 'Europe/London',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+
+
+    # Print available calendarListEntry colors.
+
+
+
+if __name__ == '__main__':
+    main()
+    setupevent()
+
